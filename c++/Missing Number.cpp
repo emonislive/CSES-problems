66d515eac3c82de4de66e542d0d84bf399365a66
Sayed Hasan <94556687+emonislive@users.noreply.github.com>
2024-07-30 23:47:58 +0600

    (comments added) this problem was pain in the heart :')

--- c++/Missing Number.cpp
+++ c++/Missing Number.cpp
@@ -13,17 +13,22 @@ int main()
     int range, input, i, miss;
     cin >> range;
     
-    for(i = 1; i <= range; i++){
+    // this vector stores all the number including missing number
+    for(i = 1; i <= range; i++){        
         v1.push_back(i);
     }
-    for (i = 0; i < range - 1; i++)
+    
+    for (i = 0; i < range - 1; i++)    
     {
+        // user input
         cin >> input;
         v2.push_back(input);
     }
+    // sorting the v2 vector that stored the user input values to compare with the v1 vector to find the missing value
     sort(v2.begin(), v2.end());
-    
-    for (i = 0; i < range; i++){
+
+    // if any of the value doesn't match with each other then value v1[x] position value is the missing
+    for (i = 0; i < range; i++){        
         if (v1[i] != v2[i]){
             miss = v1[i];
             break;
